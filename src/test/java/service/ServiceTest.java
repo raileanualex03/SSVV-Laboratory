852c9f1279cacd89595daecdf5778de4027fabab
fiamardar <stefania.mardar@gmail.com>
2022-03-08 20:04:35 +0200

    Fixed test

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -1,6 +1,13 @@
 package service;
 
+import domain.Student;
 import org.junit.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
 
 import static org.junit.Assert.assertTrue;
 
@@ -17,5 +24,29 @@ public class ServiceTest
     {
         assertTrue( true );
     }
+
+    @Test
+    public void tc_SHOULD_add_WHEN_addStudent_WITH_valid_student() {
+        Student stud = new Student("2001", "New Student", 936, "test@gmail.com");
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        service.addStudent(stud);
+
+        assert(service.findStudent(stud.getID()) != null);
+    }
+
 
 }
